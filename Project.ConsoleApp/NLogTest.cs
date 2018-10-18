cfc5b90e93a52cdde9e178344f919ed1a0d3fcc3
mahuwei <mahuwei@msn.com>
2018-10-18 19:40:53 +0800

    测试NLog。

---
+++ Project.ConsoleApp/NLogTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+namespace Project.ConsoleApp {
+    public class NLogTest {
+        private readonly ILogger<NLogTest> _logger;
+
+        public NLogTest(ILogger<NLogTest> logger) {
+            _logger = logger;
+        }
+
+        public void DoAction(string name) {
+            _logger.LogDebug(20, "Doing hard work! {Action}", name);
+        }
+
+
+        public static IServiceProvider BuildDi() {
+            var services = new ServiceCollection();
+
+            //Runner is the custom class
+            services.AddTransient<NLogTest>(); 
+            services.AddSingleton<ILoggerFactory, LoggerFactory>();
+            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+            services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            //configure NLog
+            loggerFactory.AddNLog(new NLogProviderOptions
+                {CaptureMessageTemplates = true, CaptureMessageProperties = true});
+            LogManager.LoadConfiguration("nlog.config");
+
+            return serviceProvider;
+        }
+    }
+}
--- Project.ConsoleApp/Program.cs
+++ Project.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Project.Domain;
 using Project.Infrastructure;
 
@@ -8,6 +9,16 @@ namespace Project.ConsoleApp {
     internal class Program {
         private static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            //RowVersionTest();
+            var servicesProvider = NLogTest.BuildDi();
+            var runner = servicesProvider.GetRequiredService<NLogTest>();
+            runner.DoAction("Action1");
+            Console.ReadLine();
+            // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+            NLog.LogManager.Shutdown(); 
+        }
+
+        private static void RowVersionTest() {
             var rowCount = 0;
             ulong rowFlag;
 
@@ -25,7 +36,7 @@ namespace Project.ConsoleApp {
                 }
             }
 
-            Business businessInDb; 
+            Business businessInDb;
             using (var dc = new ProjectContext()) {
                 Console.WriteLine("\n读取然后直接修改，查看行标识....");
                 businessInDb = dc.Businesses.First();
@@ -35,39 +46,42 @@ namespace Project.ConsoleApp {
 
                 businessInDb.Status++;
                 rowCount = dc.SaveChanges();
-                var rowFlagUpdate =  BitConverter.ToUInt64(businessInDb.RowFlag);
-                Console.WriteLine($"修改记录：{rowCount}行，行标识:{rowFlagUpdate}，读取时和修改后的行标识相同：{rowFlagUpdate == rowFlag}");
+                var rowFlagUpdate = BitConverter.ToUInt64(businessInDb.RowFlag);
+                Console.WriteLine(
+                    $"修改记录：{rowCount}行，行标识:{rowFlagUpdate}，读取时和修改后的行标识相同：{rowFlagUpdate == rowFlag}");
             }
 
             using (var dc = new ProjectContext()) {
                 Console.WriteLine("\n重建连接后，直接修改(DbSet.Update)，查看行标识....");
                 businessInDb.Status++;
-                var rowFlagSource =  BitConverter.ToUInt64(businessInDb.RowFlag);
+                var rowFlagSource = BitConverter.ToUInt64(businessInDb.RowFlag);
                 var changeTracking = dc.Businesses.Update(businessInDb);
                 rowCount = dc.SaveChanges();
                 rowFlag = BitConverter.ToUInt64(businessInDb.RowFlag);
-                
-                Console.WriteLine($"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和修改后的行标识相同：{rowFlagSource == rowFlag}，\n不相同说明可以使用这种办法修改，但由于修改前需要判断行标识，所以没什么意义！");
+
+                Console.WriteLine(
+                    $"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和修改后的行标识相同：{rowFlagSource == rowFlag}，\n不相同说明可以使用这种办法修改，但由于修改前需要判断行标识，所以没什么意义！");
                 rowFlag = BitConverter.ToUInt64(changeTracking.Entity.RowFlag);
-                Console.WriteLine($"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和changeTracking的行标识相同：{rowFlagSource == rowFlag}");
+                Console.WriteLine(
+                    $"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和changeTracking的行标识相同：{rowFlagSource == rowFlag}");
             }
 
             using (var dc = new ProjectContext()) {
                 Console.WriteLine("\n重建连接后，直接修改(DbSet.Update)，查看行标识....");
                 var businssSource = dc.Businesses.Find(businessInDb.Id);
                 businessInDb.Status++;
-                var rowFlagSource =  BitConverter.ToUInt64(businessInDb.RowFlag);
+                var rowFlagSource = BitConverter.ToUInt64(businessInDb.RowFlag);
                 //var changeTracking = dc.Businesses.Update(businessInDb);
                 dc.Entry(businssSource).CurrentValues.SetValues(businessInDb);
                 rowCount = dc.SaveChanges();
                 rowFlag = BitConverter.ToUInt64(businessInDb.RowFlag);
-                
-                Console.WriteLine($"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和修改后的行标识相同：{rowFlagSource == rowFlag}");
-                rowFlag = BitConverter.ToUInt64(businssSource.RowFlag);
-                Console.WriteLine($"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和(Entry(businssSource).CurrentValues.SetValues(businessInDb)的行标识相同：{rowFlagSource == rowFlag} \n 应该是不相同的，以前就一直用这种办法进行修改。");
-            }
 
-            Console.ReadLine();
+                Console.WriteLine(
+                    $"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和修改后的行标识相同：{rowFlagSource == rowFlag}");
+                rowFlag = BitConverter.ToUInt64(businssSource.RowFlag);
+                Console.WriteLine(
+                    $"修改记录：{rowCount}行，行标识:{rowFlagSource}，读取时和(Entry(businssSource).CurrentValues.SetValues(businessInDb)的行标识相同：{rowFlagSource == rowFlag} \n 应该是不相同的，以前就一直用这种办法进行修改。");
+            }
         }
     }
 }

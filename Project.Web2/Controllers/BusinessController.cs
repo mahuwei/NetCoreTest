a82183e881fb3549e42278a76ba48037439278f6
mahuwei <mahuwei@msn.com>
2018-11-08 17:45:53 +0800

    添加Serilog日志测试。

--- Project.Web2/Controllers/BusinessController.cs
+++ Project.Web2/Controllers/BusinessController.cs
@@ -1,17 +1,22 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Project.Domain;
 
 namespace Project.Web2.Controllers {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class BusinessController : ControllerBase {
         private readonly IMediator _mediator;
+        private readonly ILogger<BusinessController> _logger;
 
-        public BusinessController(IMediator mediator) {
+        public BusinessController(IMediator mediator,
+            ILogger<BusinessController> logger) {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpPost]
@@ -22,5 +27,25 @@ namespace Project.Web2.Controllers {
 
             return Ok(response.Result);
         }
+
+        [HttpPost]
+        public IActionResult LogTest([FromBody] TempData data) {
+            _logger.LogInformation("LogInformation,data:{@data}", data);
+            try {
+                throw new Exception("test error.");
+            }
+            catch (Exception e) {
+                _logger.LogError(e, "LogError,data:{@data}", data);
+            }
+
+            _logger.LogWarning("LogWarning,data:{@data}", data);
+            _logger.LogDebug("LogDebug,Data:{@data}", data);
+            return Ok(data);
+        }
+    }
+
+    public class TempData {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- Project.Web2/Program.cs
+++ Project.Web2/Program.cs
@@ -1,21 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Project.Web2 {
     public class Program {
+        public static IConfiguration Configuration { get; } =
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                    true)
+                .AddEnvironmentVariables()
+                .Build();
+
         public static void Main(string[] args) {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            try {
+                Log.Information("Getting the motors running...");
+
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex) {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseConfiguration(Configuration)
+                .UseSerilog();
+        }
     }
 }
